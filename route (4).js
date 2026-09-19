1a95842c10d39161121bf6e9d73692a9934456a8
adixmain228-del <adixmain228@gmail.com>
2026-09-19 14:28:46 +0500

    Add files via upload

---
+++ route (4).js
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+import { createRoom } from "@/lib/store";
+
+export async function POST(request) {
+  const body = await request.json().catch(() => ({}));
+  const name = typeof body.name === "string" ? body.name : "";
+  const code = await createRoom(name);
+  return NextResponse.json({ code });
+}
