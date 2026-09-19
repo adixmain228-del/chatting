604535d5461ed662052bbeaeba979544a37bd994
adixmain228-del <adixmain228@gmail.com>
2026-09-19 14:34:35 +0500

    Delete route (4).js

--- route (4).js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { createRoom } from "@/lib/store";
-
-export async function POST(request) {
-  const body = await request.json().catch(() => ({}));
-  const name = typeof body.name === "string" ? body.name : "";
-  const code = await createRoom(name);
-  return NextResponse.json({ code });
-}
